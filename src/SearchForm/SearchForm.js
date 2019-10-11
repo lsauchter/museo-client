09a4c5bb1767c11b350c1585f8370571c3b2e7b0
Lauren Auchter <lsauchter@gmail.com>
2019-10-10 20:34:57 -0400

    Created search form with MapBox geocode function

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,16 +1,29 @@
-import React from 'react'
+//File is an edited copy of this node module
+//https://github.com/kmwhelan93/react-geocoder-autocomplete
+//This version below includes the country query to limit results to one country
+
+import React, {useContext} from 'react'
+import MuseumContext from '../MuseumContext'
+import  Geocoder from '../Geocoder/Geocoder'
 import './SearchForm.css'
 
 export default function SearchForm() {
+    const {viewport, setViewport} = useContext(MuseumContext)
+    const mapAccess = {
+        accessToken: process.env.REACT_APP_API_TOKEN
+    }
+
+    const onSelect = (res) => {
+        //setViewport(viewport)
+        console.log(res)
+    }
+
     return(
-        <form>
-            <input
-                id="search"
-                name="search"
-                type="text"
-                placeholder="City, Address, ZIP"
-            />
-            <button type="submit">Search</button>
-		</form>
+        <Geocoder
+            {...mapAccess}
+            onSelect={(res) => onSelect(res)}
+            country='US'
+            inputPlaceholder='City, Address, ZIP'
+        />
     )
 }
