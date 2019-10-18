970c00712b418b95fafc6c0b2b80bc14f97ad91c
Lauren Auchter <lsauchter@gmail.com>
2019-10-17 23:20:56 -0400

    Functional filters and filter menu, basic styling

--- src/MapFilters/MapFilters.js
+++ src/MapFilters/MapFilters.js
@@ -1,74 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './MapFilters.css'
 
-export default function MapFilters() {
+export default function MapFilters(props) {
+    const [filterMenu, updateFilterMenu] = useState(false)
+
+    function handleClick(abbr) {
+        props.setFilters(abbr)
+    }
+
+    function handleFilterMenu() {
+        updateFilterMenu(!filterMenu)
+    }
+    
+    const { ART, CMU, BOT, GMU, HST, HSC, NAT, SCI, ZAW } = props.filter
+
     return(
-        // <ul className="filters">
-        //     <li>
-        //         <button>Art Museums</button>
-        //     </li>
-        //     <li>
-        //         <button>Gardens & Nature Centers</button>
-        //     </li>
-        //     <li>
-        //         <button>Children's Museums</button>
-        //     </li>
-        //     <li>
-        //         <button>History Museums</button>
-        //     </li>
-        //     <li>
-        //         <button>Natural History & Natural Science Museums</button>
-        //     </li>
-        //     <li>
-        //         <button>Science Museums & Planetariums</button>
-        //     </li>
-        //     <li>
-        //         <button>Zoos and Aquariums</button>
-        //     </li>
-        //     <li>
-        //         <button>General Museums</button>
-        //     </li>
-        //     <li>
-        //         <button>Historical Societies & Preservation</button>
-        //     </li>
-        // </ul>
-        <ul className="filters">
-        <li>
-            <input type="checkbox" id="art"/>
-            <label htmlFor="art">Art Museums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="bot"/>
-            <label htmlFor="bot">Gardens & Nature Centers</label>
-        </li>
-        <li>
-            <input type="checkbox" id="child" />
-            <label htmlFor="child">Children's Museums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="hist"/>
-            <label htmlFor="hist">History Museums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="nat"/>
-            <label htmlFor="nat">Natural History & Natural Science Museums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="sci"/>
-            <label htmlFor="sci">Science Museums & Planetariums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="zoo"/>
-            <label htmlFor="zoo">Zoos and Aquariums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="mus"/>
-            <label htmlFor="mus">General Museums</label>
-        </li>
-        <li>
-            <input type="checkbox" id="soc"/>
-            <label htmlFor="soc">Historical Societies & Preservation</label>
-        </li>
-    </ul>
+        <>
+        <button className="button--filter" onClick={() => handleFilterMenu()}>Filter</button>
+        {filterMenu && <ul className="filters">
+            <li>
+                <button onClick={() => handleClick('ART')}>
+                    {ART && <FontAwesomeIcon className='check' icon='check'/>}
+                    Art Museums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('CMU')}>
+                    {CMU && <FontAwesomeIcon className='check' icon='check'/>}
+                    Children's Museums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('BOT')}>
+                    {BOT && <FontAwesomeIcon className='check' icon='check'/>}
+                    Gardens & Nature Centers
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('GMU')}>
+                    {GMU && <FontAwesomeIcon className='check' icon='check'/>}
+                    General Museums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('HST')}>
+                    {HST && <FontAwesomeIcon className='check' icon='check'/>}
+                    History Museums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('HSC')}>
+                    {HSC && <FontAwesomeIcon className='check' icon='check'/>}
+                    Historical Societies & Preservation
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('NAT')}>
+                    {NAT && <FontAwesomeIcon className='check' icon='check'/>}
+                    Natural History & Natural Science Museums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('SCI')}>
+                    {SCI && <FontAwesomeIcon className='check' icon='check'/>}
+                    Science Museums & Planetariums
+                </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick('ZAW')}>
+                    {ZAW && <FontAwesomeIcon className='check' icon='check'/>}
+                    Zoos and Aquariums
+                </button>
+            </li>
+        </ul>
+        }
+        </>
     )
 }
--- src/MapPage/MapPage.js
+++ src/MapPage/MapPage.js
@@ -6,22 +6,46 @@ import './MapPage.css'
 
 export default function MapPage() {
     const [museum, updateMuseum] = useState({})
+    const [filter, updateFilter] = useState({
+        ART: true,
+        CMU: true,
+        BOT: true,
+        GMU: true,
+        HST: true,
+        HSC: true,
+        NAT: true,
+        SCI: true,
+        ZAW: true
+    })
 
     function setMuseumResult(museum) {
         updateMuseum(museum)
     }
 
+    function setFilters(abbr) {
+        const update = {
+            ...filter,
+            [abbr]: !filter[abbr]
+        }
+        updateFilter(update)
+    }
+
     return(
         <>
         <div className="map">
-            <Map setMuseumResult={(museum) => setMuseumResult(museum)}/>
+            <Map
+                setMuseumResult={(museum) => setMuseumResult(museum)}
+                filter={filter}/>
+            {(Object.keys(museum).length > 0) && <div className="bottom result">
+                <MuseumResult museum={museum}/>
+                </div>}
         </div>
-        <div className="top">
-            <MapFilters />
+        <div className="filterContainer">
+            <MapFilters 
+                setFilters={(abbr) => setFilters(abbr)}
+                filter={filter}/>
         </div>
-        <div className="bottom result">
-            <MuseumResult museum={museum}/>
-        </div>
+        
 
         </>
     )
