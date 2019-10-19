88a5fa8eec036541951ce23cba52d6849449cb3c
Lauren Auchter <lsauchter@gmail.com>
2019-10-18 21:12:42 -0400

    Get user location and search for museums

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react'
 import {withRouter} from 'react-router'
 import MuseumContext from '../MuseumContext'
 import  Geocoder from '../Geocoder/Geocoder'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './SearchForm.css'
 
 function SearchForm(props) {
@@ -18,15 +19,41 @@ function SearchForm(props) {
         setCenter(res.center)
     }
 
+    function getUserLocation() {
+        if (props.activeLink === "true") {
+            props.history.push('/map')
+        }
+        //The if...in checks for geolocation support within the browser
+        //Users choose to allow (success) or deny (error) only IF geolcation is supported
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                function success(position) {
+                    setCenter([position.coords.longitude, position.coords.latitude]);
+                },
+                function error() {
+                    console.log('location denied');
+                }
+            ); 
+        }
+        else {
+            console.log('geolocation is not enabled on this browser');
+        }
+    }
+
     return(
+        <>
         <Geocoder
             {...mapAccess}
             onSelect={(res) => onSelect(res)}
             country='US'
-            inputPlaceholder='City, Address, ZIP'
+            inputPlaceholder='Address, City, or ZIP'
             resultClass='geocoder__li'
             resultsClass='geocoder__ul'
         />
+        <button onClick={() => getUserLocation()}>
+            <FontAwesomeIcon icon="location-arrow" />
+        </button>
+        </>
     )
 }
 
