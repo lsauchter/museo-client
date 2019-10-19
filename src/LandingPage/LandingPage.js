21f92a566ddc6403305eafbb434e69dc5ae649ec
Lauren Auchter <lsauchter@gmail.com>
2019-10-18 21:12:42 -0400

    Get user location and search for museums

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -8,7 +8,9 @@ export default function LandingPage() {
         <div className="container hero">
             <h2>Find a museum</h2>
             <p>Discover museums nearby or wherever your travels take you</p>
-            <SearchForm activeLink="true"/>
+            <div className="landingPageSearch">
+                <SearchForm activeLink="true"/>
+            </div>
 		</div>
 		<div className="container top">
             <h3>Museums are everywhere</h3>
