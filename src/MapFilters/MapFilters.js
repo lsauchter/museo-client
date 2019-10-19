200f064f6c0dcf3ab60c2b77a2b23ed5fa62b393
Lauren Auchter <lsauchter@gmail.com>
2019-10-18 22:30:50 -0400

    Added icons to the filters

--- src/MapFilters/MapFilters.js
+++ src/MapFilters/MapFilters.js
@@ -22,54 +22,63 @@ export default function MapFilters(props) {
             <li>
                 <button onClick={() => handleClick('ART')}>
                     {ART && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='palette' />
                     Art Museums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('CMU')}>
                     {CMU && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='child' />
                     Children's Museums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('BOT')}>
                     {BOT && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='leaf' />
                     Gardens & Nature Centers
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('GMU')}>
                     {GMU && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='landmark' />
                     General Museums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('HST')}>
                     {HST && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='university' />
                     History Museums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('HSC')}>
                     {HSC && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='university' />
                     Historical Societies & Preservation
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('NAT')}>
                     {NAT && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='bone' />
                     Natural History & Natural Science Museums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('SCI')}>
                     {SCI && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='flask' />
                     Science Museums & Planetariums
                 </button>
             </li>
             <li>
                 <button onClick={() => handleClick('ZAW')}>
                     {ZAW && <FontAwesomeIcon className='check' icon='check'/>}
+                    <FontAwesomeIcon className="museumKey" icon='hippo' />
                     Zoos and Aquariums
                 </button>
             </li>
