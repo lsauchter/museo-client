60869a583ed19986d516b6826bf0c25c56eab082
Lauren Auchter <lsauchter@gmail.com>
2019-10-21 17:02:40 -0400

    Finalized map logic and using env variables

--- src/MuseumResult/MuseumResult.js
+++ src/MuseumResult/MuseumResult.js
@@ -3,25 +3,21 @@ import './MuseumResult.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function MuseumResult(props) {
-    const {COMMONNAME, PHONE, WEBURL, GSTREET, GCITY, GSTATE, GZIP5} = props.museum
-    let weburl
-    if (WEBURL) {
-        weburl = WEBURL.toLowerCase()
-    }
+    const {commonname, phone, weburl, gstreet, gcity, gstate, gzip5} = props.museum
 
     return(
         <>
         <button onClick={() => props.setMuseumResult({})} className="times">
             <FontAwesomeIcon icon="times" />
         </button>
-        <h3>{COMMONNAME}</h3>
-        {PHONE && <p>phone: {PHONE}</p>}
-        {WEBURL && <div className="websiteLink">
-            <p>website: </p><a href={WEBURL} target="blank">{weburl}</a>
+        <h3>{commonname}</h3>
+        {phone && <p>phone: {phone}</p>}
+        {weburl && <div className="websiteLink">
+            <p>website: </p><a href={weburl} target="blank">{weburl}</a>
         </div>}
-        {GSTREET && <section name="address">
-            <p>{GSTREET}</p>
-            <p>{GCITY}, {GSTATE}, {GZIP5}</p>
+        {(gstreet || gcity) && <section name="address">
+            <p>{gstreet}</p>
+            <p>{gcity}, {gstate}, {gzip5}</p>
         </section>}
         </>
     )
