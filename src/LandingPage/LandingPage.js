d811d38417d29dea4918d3db8159ca138b3e9fcf
Lauren Auchter <lsauchter@gmail.com>
2019-10-23 16:15:04 -0400

    Updated readme and relocated location button

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,24 +4,24 @@ import './LandingPage.css'
 
 export default function LandingPage() {
     return(
-        <>
+        <div className="landingPage">
         <div className="container hero">
-            <h2>Find a museum</h2>
-            <p>Discover museums nearby or wherever your travels take you</p>
-            <div className="landingPageSearch">
-                <SearchForm activeLink="true"/>
+            <div className="hero__text">
+                <h2>Find a museum</h2>
+                <p>Discover museums nearby or wherever your travels take you</p>
+                <div className="landingPageSearch">
+                    <SearchForm activeLink="true"/>
+                </div>
             </div>
 		</div>
-        <div className="information">
-            <div className="container top">
-                <h3>Museums are everywhere</h3>
-                <p>There are over <span className="bold">30,000</span> museums in the US so find something new to explore! Play tourist at home or find something unique to do while you're traveling. Either way, museo can help you plan.</p>
-            </div>
-            <div className="container bottom">
-                <h3>How it works</h3>
-                <p>Enter an address or use your location to search for museums. Filter by the type of museum - zoo, history, art, and more - to find exactly what you're looking for.</p>
-            </div>
+        <div className="container top">
+            <h3>Museums are everywhere</h3>
+            <p>There are over <span className="bold">30,000</span> museums in the US so find something new to explore! Play tourist at home or find something unique to do while you're traveling. Either way, museo can help you plan.</p>
         </div>
-        </>
+        <div className="container bottom">
+            <h3>How it works</h3>
+            <p>Enter an address or use your location to search for museums. Filter by the type of museum - zoo, history, art, and more - to find exactly what you're looking for.</p>
+        </div>
+    </div>
     )
 }
