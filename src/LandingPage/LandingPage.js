45f81986f8240e9eb0a5a8000853f0b4e43b5b61
Lauren Auchter <lsauchter@gmail.com>
2019-10-23 22:43:24 -0400

    Landing page content layout adjustment

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchForm from '../SearchForm/SearchForm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './LandingPage.css'
 
 export default function LandingPage() {
@@ -14,11 +15,10 @@ export default function LandingPage() {
                 </div>
             </div>
 		</div>
-        <div className="container top">
+        <div className="container bottom">
             <h3>Museums are everywhere</h3>
             <p>There are over <span className="bold">30,000</span> museums in the US so find something new to explore! Play tourist at home or find something unique to do while you're traveling. Either way, museo can help you plan.</p>
-        </div>
-        <div className="container bottom">
+            <FontAwesomeIcon icon="university" size='2x' className="landingIcon"/>
             <h3>How it works</h3>
             <p>Enter an address or use your location to search for museums. Filter by the type of museum - zoo, history, art, and more - to find exactly what you're looking for.</p>
         </div>
